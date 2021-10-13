59043d60ad356fa9e4bf8baf3bcf1e112e1beffd
swapnilpathak9 <86046197+swapnilpathak9@users.noreply.github.com>
2021-10-13 14:16:27 +0530

    Delete sampleCRUD directory

--- sampleCRUD/src/main/java/com/sample/sampleCRUD/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sample.sampleCRUD.repository;
-
-import com.sample.sampleCRUD.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    Product findByName(String name);
-}
-
